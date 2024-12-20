ea42fae8d3b55d18ff935790c490b01a56246e67
hxse <narutohy@163.com>
2024-12-21 01:17:11 +0800

    update

--- assets/js/codebutton.js
+++ assets/js/codebutton.js
@@ -1,9 +1,8 @@
-// assets/js/post.js
 var codeBlocks = document.querySelectorAll('pre.highlight');
 
 codeBlocks.forEach(function (codeBlock) {
     var copyButton = document.createElement('button');
-    copyButton.className = 'copy';
+    copyButton.className = 'copy btn'; // btn은 minimal mistake에서 지원하는 버튼을 사용하기 위해 추가 하였다.
     copyButton.type = 'button';
     copyButton.ariaLabel = 'Copy code to clipboard';
     copyButton.innerText = 'Copy';
@@ -11,12 +10,12 @@ codeBlocks.forEach(function (codeBlock) {
     codeBlock.append(copyButton);
 
     copyButton.addEventListener('click', function () {
-        var code = codeBlock.querySelector('code').innerText.trim();
+        var code = codeBlock.querySelector('.rouge-code').innerText.trim();
         window.navigator.clipboard.writeText(code);
 
         copyButton.innerText = 'Copied';
-        var fourSeconds = 4000;
 
+        var fourSeconds = 4000;
         setTimeout(function () {
             copyButton.innerText = 'Copy';
         }, fourSeconds);
